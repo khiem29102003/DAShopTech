4b7b53427d085894f5017513e839e800cd4c6eee
KHIÊM NGÔ <ngokhiem183@gmail.com>
2026-07-15 23:10:01 +0700

    Add files via upload

---
+++ js/chatbot.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("chatbot-send").addEventListener("click", function () {
+    var userInput = document.getElementById("chatbot-input").value;
+
+    fetch('/ChatBot/GetResponse', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userInput)
+    })
+        .then(response => response.json())
+        .then(data => {
+            var messageContainer = document.getElementById("chatbot-messages");
+            messageContainer.innerHTML += `<div class="bot-message">${data.response}</div>`;
+        });
+});
